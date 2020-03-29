0e9f42d00ed491609f1e7b8f610185c373e29cba
nazkord <nazkord@gmail.com>
2020-03-30 01:11:20 +0200

    -added resources

--- src/main/java/com/dbteam/MoneySplittingBot.java
+++ src/main/java/com/dbteam/MoneySplittingBot.java
@@ -6,14 +6,18 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.io.IOException;
+
 public class MoneySplittingBot extends TelegramLongPollingBot {
+    
+    public static final String BOT_TOKEN_FILENAME = "telegramBotToken.txt";
+    public static final String BOT_USERNAME_FILENAME = "telegramBotUsername.txt";
+
     public void onUpdateReceived(Update update) {
 
         //here the magic happens ;)
         Message received = update.getMessage();
         SendMessage newMessage = new SendMessage(received.getChatId(), received.getText());
-
-
 
         try {
             execute(newMessage);
@@ -24,10 +28,26 @@ public class MoneySplittingBot extends TelegramLongPollingBot {
     }
 
     public String getBotUsername() {
-        return "money_splitting_bot";
+        return getBotUsernameFromProperties();
     }
 
     public String getBotToken() {
-        return "907887577:AAHWZys6_BZgJk2xksJkt6urXujcbFby8is";
+        return getBotTokenFromProperties();
+    }
+
+    private String getBotTokenFromProperties() {
+        try {
+            return new String(MoneySplittingBot.class.getResourceAsStream("/" + BOT_TOKEN_FILENAME).readAllBytes());
+        } catch (IOException e) {
+            throw new RuntimeException("Exception while reading from resources");
+        }
+    }
+
+    private String getBotUsernameFromProperties() {
+        try {
+            return new String(MoneySplittingBot.class.getResourceAsStream("/" + BOT_USERNAME_FILENAME).readAllBytes());
+        } catch (IOException e) {
+            throw new RuntimeException("Exception while reading from resources");
+        }
     }
 }
