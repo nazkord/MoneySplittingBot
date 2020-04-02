2dc96cf7d97076bfc423c26bce406662a55ad942
szelemeh <subzarro6@gmail.com>
2020-04-02 20:28:12 +0300

    add better properties handling

--- src/main/java/com/dbteam/Main.java
+++ src/main/java/com/dbteam/Main.java
@@ -1,18 +1,28 @@
 package com.dbteam;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.bson.Document;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 public class Main {
+    private static final String DATABASE_NAME = "botdb";
+
     public static void main(String[] args) throws TelegramApiRequestException {
 
         configureLog4j();
 
         ApiContextInitializer.init();
+
+        MongoDatabase db = getMongoDatabase();
+        db.getCollection("users").insertOne(new Document("username", "szelemeh")
+                .append("groupId", 1));
 
         MoneySplittingBot mBot = new MoneySplittingBot();
 
@@ -23,6 +33,15 @@ public class Main {
         } catch (TelegramApiException e) {
             throw new TelegramApiRequestException("Error while registering the bot the telegram API");
         }
+    }
+
+    private static MongoDatabase getMongoDatabase() {
+        String username = PropertyHelper.getMongoUsername();
+        String password = PropertyHelper.getMongoPassword();
+        MongoClient mongoClient = MongoClients.create(
+                "mongodb+srv://"+username+":"+password+
+                        "@moneybot-qwdm2.mongodb.net/test?retryWrites=true&w=majority");
+        return mongoClient.getDatabase(DATABASE_NAME);
     }
 
     private static void configureLog4j() {
--- src/main/java/com/dbteam/MoneySplittingBot.java
+++ src/main/java/com/dbteam/MoneySplittingBot.java
@@ -6,12 +6,7 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.io.IOException;
-
 public class MoneySplittingBot extends TelegramLongPollingBot {
-    
-    public static final String BOT_TOKEN_FILENAME = "telegramBotToken.txt";
-    public static final String BOT_USERNAME_FILENAME = "telegramBotUsername.txt";
 
     public void onUpdateReceived(Update update) {
 
@@ -24,30 +19,13 @@ public class MoneySplittingBot extends TelegramLongPollingBot {
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
-
     }
 
     public String getBotUsername() {
-        return getBotUsernameFromProperties();
+        return PropertyHelper.getBotUsername();
     }
 
     public String getBotToken() {
-        return getBotTokenFromProperties();
-    }
-
-    private String getBotTokenFromProperties() {
-        try {
-            return new String(MoneySplittingBot.class.getResourceAsStream("/" + BOT_TOKEN_FILENAME).readAllBytes());
-        } catch (IOException e) {
-            throw new RuntimeException("Exception while reading from resources");
-        }
-    }
-
-    private String getBotUsernameFromProperties() {
-        try {
-            return new String(MoneySplittingBot.class.getResourceAsStream("/" + BOT_USERNAME_FILENAME).readAllBytes());
-        } catch (IOException e) {
-            throw new RuntimeException("Exception while reading from resources");
-        }
+        return PropertyHelper.getBotToken();
     }
 }
