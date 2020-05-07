5520e639640c2fa37c0a3dd514d62214825cd710
szelemeh <subzarro6@gmail.com>
2020-05-07 21:47:08 +0300

    Added first replies

--- src/main/java/com/dbteam/MoneySplittingBot.java
+++ src/main/java/com/dbteam/MoneySplittingBot.java
@@ -1,7 +1,13 @@
 package com.dbteam;
 
+import com.dbteam.controller.abilities.AddPaymentAbility;
+import com.dbteam.controller.replies.BotAddedToGroupReply;
+import com.dbteam.controller.replies.NewGroupMemberReply;
 import org.springframework.stereotype.Component;
 import org.telegram.abilitybots.api.bot.AbilityBot;
+import org.telegram.abilitybots.api.objects.Ability;
+import org.telegram.abilitybots.api.objects.Reply;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 
 @Component
 public class MoneySplittingBot extends AbilityBot {
@@ -17,5 +23,16 @@ public class MoneySplittingBot extends AbilityBot {
     public int creatorId() {
         return creatorId;
     }
-    
+
+    public Ability AddPaymentAbility() {
+        return new AddPaymentAbility(silent, this).get();
+    }
+
+    public Reply BotAddedToGroupReply() {
+        return new BotAddedToGroupReply(silent, this).get();
+    }
+
+    public Reply NewGroupMemberReply() {
+        return new NewGroupMemberReply(silent, this).get();
+    }
 }
