d01de3b928a5755776a34964cdb4524db7a3ce71
szelemeh <subzarro6@gmail.com>
2020-05-12 19:32:40 +0300

    Added Start ability handler

---
+++ src/main/java/com/dbteam/controller/ability/CommandHandler.java
@@ -0,0 +1,22 @@
+package com.dbteam.controller.ability;
+
+
+import com.dbteam.model.Command;
+import com.dbteam.model.ReplyHolder;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.util.List;
+
+/**
+ * Handles user commands like /start, /help and so on.
+ */
+public interface CommandHandler {
+
+    SendMessage primaryAction(Update update);
+
+    List<BotApiMethod<?>> secondaryAction(Update update);
+
+    Command commandToHandle();
+}
---
+++ src/main/java/com/dbteam/controller/ability/StartHandler.java
@@ -0,0 +1,89 @@
+package com.dbteam.controller.ability;
+
+import com.dbteam.exception.PersonNotFoundException;
+import com.dbteam.model.Command;
+import com.dbteam.model.Person;
+import com.dbteam.model.ReplyHolder;
+import com.dbteam.service.GroupService;
+import com.dbteam.service.PersonService;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class StartHandler implements CommandHandler{
+
+    private static final String MSG_START =
+            "Hello! I am MoneySplittingBot. I can remember all your group expenses. ";
+    private static final String MSG_NO_GROUPS =
+            "Add me to your group chat and I will show you what I can!";
+
+    private final GroupService groupService;
+    private final PersonService personService;
+
+    public StartHandler(GroupService groupService, PersonService personService) {
+        this.groupService = groupService;
+        this.personService = personService;
+    }
+
+    @Override
+    public SendMessage primaryAction(Update update) {
+        StringBuilder reply = new StringBuilder();
+        reply.append(MSG_START);
+        Person person;
+
+        try {
+            person = personService.findPersonByUsername(getUsername(update));
+        } catch (PersonNotFoundException e) {
+            person = getPersonFrom(update);
+            personService.addPerson(person);
+        }
+
+        Map<Long, String> groupChatStates = person.getGroupChatsStates();
+
+        if (groupChatStates == null || groupChatStates.isEmpty()) {
+            reply.append(MSG_NO_GROUPS);
+        } else {
+            reply
+                    .append("You have ")
+                    .append(person.getGroupChatsStates().size())
+                    .append(" groups. Type '/' to see available commands.");
+        }
+
+        SendMessage message = new SendMessage();
+        message.setText(reply.toString());
+        message.setChatId(update.getMessage().getChatId());
+
+        person.setChatId(update.getMessage().getChatId());
+        personService.addPerson(person);
+
+        return message;
+    }
+
+    private String getUsername(Update update) {
+        return update.getMessage().getFrom().getUserName();
+    }
+
+    private Person getPersonFrom(Update update) {
+        User user = update.getCallbackQuery().getFrom();
+        return new Person(user.getUserName(),
+                user.getFirstName() + " " + user.getLastName(),
+                update.getMessage().getChatId(), new HashMap<>(),
+                null);
+    }
+
+    @Override
+    public List<ReplyHolder> getReplyHolders(Update update) {
+        return null;
+    }
+
+    @Override
+    public Command commandToHandle() {
+        return Command.START;
+    }
+}
