38b9a88324406f0d14b958d91ac028afdf1ac379
szelemeh <subzarro6@gmail.com>
2020-05-18 00:05:02 +0300

    Refactored CheckPaymentsHandler

--- src/main/java/com/dbteam/controller/ability/CheckPaymentsHandler.java
+++ src/main/java/com/dbteam/controller/ability/CheckPaymentsHandler.java
@@ -23,6 +23,7 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMa
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
@@ -39,7 +40,6 @@ public class CheckPaymentsHandler implements CommandHandler {
             "No more payments.";
     private static final String MSG_NO_PAYMENTS_AT_ALL =
             "You don't have any payments yet.";
-
     private static final String LBL_OLDER_PAYMENT =
             "Older";
     private static final String LBL_NEWER_PAYMENT =
@@ -54,6 +54,10 @@ public class CheckPaymentsHandler implements CommandHandler {
     private final PaymentService paymentService;
     private final StateService stateService;
 
+    private Update currentUpdate;
+    private Payment currentPayment;
+    private Person currentPerson;
+
     public CheckPaymentsHandler(GroupService groupService,
                                 PersonService personService,
                                 PaymentService paymentService,
@@ -67,36 +71,68 @@ public class CheckPaymentsHandler implements CommandHandler {
     @Override
     public List<BotApiMethod<?>> primaryAction(Update update) {
         List<BotApiMethod<?>> apiMethods = new ArrayList<>();
+
+        try {
+            currentPerson = personService.findPersonByUsername(getUsername(update));
+        } catch (PersonNotFoundException e) {
+            e.printStackTrace();
+        }
+
         SendMessage message = new SendMessage();
         message.setText(MSG_PRIMARY);
-        message.setChatId(getChatId(update));
+        message.setChatId(currentPerson.getChatId());
 
-        Person person;
-        try {
-            person = personService.findPersonByUsername(getUsername(update));
-        } catch (PersonNotFoundException e) {
-            e.printStackTrace();
-            message.setText(MSG_ERROR);
-            apiMethods.add(message);
-            return apiMethods;
-        }
-
-        try {
-            personService.updatePersonBotChatState(person.getUsername(),
-                    stateService.buildBotChatState("checkpayments"));
-        } catch (PersonNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        if (person.getGroupChatsStates() == null || person.getGroupChatsStates().isEmpty()) {
+        if (currentPerson.getGroupChatsStates() == null || currentPerson.getGroupChatsStates().isEmpty()) {
             message.setText(MSG_NO_GROUPS);
             apiMethods.add(message);
             return apiMethods;
         }
 
+        try {
+            currentPayment = paymentService.getFirstPaymentWithUserBefore(LocalDate.now(), getUsername(update));
+        } catch (PaymentNotFoundException e) {
+            e.printStackTrace();
+            message.setText(MSG_NO_PAYMENTS_AT_ALL);
+            apiMethods.add(message);
+            return apiMethods;
+        }
+
+        updatePersonBotChatState(currentPayment.getPaymentId().toString());
         apiMethods.add(message);
-        apiMethods.addAll(loadOlderPayment(update, "checkpayments"));
+        apiMethods.add(displayPaymentInfo());
         return apiMethods;
+    }
+
+    private SendMessage displayPaymentInfo() {
+        String groupTitle = getGroupTitle(currentPayment);
+        SendMessage message = new SendMessage(
+                currentPerson.getChatId(),
+                GeneralPaymentInfoBuilder.of(currentPayment, groupTitle)
+        );
+        message.setReplyMarkup(getKeyboardMarkup());
+        return message;
+    }
+
+    private InlineKeyboardMarkup getKeyboardMarkup() {
+        InlineKeyboardMarkupBuilder builder = new InlineKeyboardMarkupBuilder();
+        if (currentPayment.getRecipient().equals(currentPerson.getUsername())) {
+            String label = currentPayment.getIsConfirmed() ? LBL_UNCONFIRM : LBL_CONFIRM;
+            builder.setButton(
+                    0, 0,
+                    label,
+                    CallbackData.TOGGLE_CONFIRMATION +
+                            getPaymentIdPart(currentPayment));
+        }
+        builder
+                .setButton(1, 0,
+                        LBL_NEWER_PAYMENT,
+                        CallbackData.LOAD_NEWER_PAYMENT.getValue()
+                                + getPaymentIdPart(currentPayment))
+                .setButton(1, 1,
+                        LBL_OLDER_PAYMENT,
+                        CallbackData.LOAD_OLDER_PAYMENT.getValue()
+                                + getPaymentIdPart(currentPayment));
+        return builder.build();
     }
 
     @Override
@@ -113,90 +149,31 @@ public class CheckPaymentsHandler implements CommandHandler {
             e.printStackTrace();
             SendMessage sendMessage = new SendMessage();
             sendMessage.setText(MSG_ERROR);
-            sendMessage.setChatId(getChatId(update));
+            sendMessage.setChatId(currentPerson.getChatId());
             apiMethods.add(sendMessage);
             return apiMethods;
         }
-        String chatState;
+        Long currentPaymentId = Long.valueOf(getCallbackDataPostfix(callbackQuery.getData()));
+        currentUpdate = update;
         try {
-            chatState = stateService.getBotChatStateOfUser(getUsername(update));
-        } catch (PersonNotFoundException e) {
+            currentPayment = paymentService.getPaymentById(currentPaymentId);
+            currentPerson = personService.findPersonByUsername(getUsername(update));
+        } catch (PaymentNotFoundException | PersonNotFoundException e) {
             e.printStackTrace();
-            SendMessage sendMessage = new SendMessage();
-            sendMessage.setText(MSG_ERROR);
-            sendMessage.setChatId(getChatId(update));
-            apiMethods.add(sendMessage);
-            return apiMethods;
         }
 
         switch (data) {
             case LOAD_OLDER_PAYMENT:
-                apiMethods.addAll(loadOlderPayment(update, chatState));
-                break;
+                return loadOlderPayment();
             case LOAD_NEWER_PAYMENT:
-                apiMethods.addAll(loadNewerPayment(update, chatState));
-                break;
+                return loadNewerPayment();
             case TOGGLE_CONFIRMATION:
-                apiMethods.addAll(toggleConfirmation(update, chatState));
-                break;
+                return toggleConfirmation();
             case LOAD_PREVIOUS_PAYMENT:
-                apiMethods.addAll(loadPreviousPayment(update, chatState));
-                break;
-
+                return loadPreviousPayment();
+            default:
+                return apiMethods;  // returning empty array
         }
-
-        return apiMethods;
-    }
-
-    @SneakyThrows
-    private List<BotApiMethod<?>> loadPreviousPayment(Update update, String chatState) {
-        List<BotApiMethod<?>> apiMethods = new ArrayList<>();
-        String[] stateParts = chatState.split("/");
-        Payment payment;
-
-        payment = paymentService.getPaymentById(Long.valueOf(stateParts[1]));
-
-        EditMessageText editText = new EditMessageText();
-        editText
-                .setChatId(getChatId(update))
-                .setText(GeneralPaymentInfoBuilder.of(payment, getGroupTitle(payment)))
-                .setMessageId(update.getCallbackQuery().getMessage().getMessageId());
-        EditMessageReplyMarkup editMarkup = new EditMessageReplyMarkup();
-        editMarkup
-                .setReplyMarkup(getNormalButtons(update, payment))
-                .setInlineMessageId(update.getCallbackQuery().getInlineMessageId())
-                .setChatId(getChatId(update))
-                .setMessageId(update.getCallbackQuery().getMessage().getMessageId());
-        apiMethods.add(editText);
-        apiMethods.add(editMarkup);
-        return apiMethods;
-
-    }
-
-    @SneakyThrows
-    private List<BotApiMethod<?>> toggleConfirmation(Update update, String chatState) {
-        List<BotApiMethod<?>> apiMethods = new ArrayList<>();
-        String[] stateParts = chatState.split("/");
-        Payment payment;
-
-        payment = paymentService.getPaymentById(Long.valueOf(stateParts[1]));
-
-        payment.setIsConfirmed(!payment.getIsConfirmed());
-        paymentService.updatePayment(payment);
-        EditMessageText editText = new EditMessageText();
-        editText
-                .setChatId(getChatId(update))
-                .setText(GeneralPaymentInfoBuilder.of(payment, getGroupTitle(payment)))
-                .setMessageId(update.getCallbackQuery().getMessage().getMessageId());
-        EditMessageReplyMarkup editMarkup = new EditMessageReplyMarkup();
-        editMarkup
-                .setReplyMarkup(getNormalButtons(update, payment))
-                .setInlineMessageId(update.getCallbackQuery().getInlineMessageId())
-                .setChatId(getChatId(update))
-                .setMessageId(update.getCallbackQuery().getMessage().getMessageId());
-        apiMethods.add(editText);
-        apiMethods.add(editMarkup);
-        return apiMethods;
     }
 
     private CallbackData getCallbackDataPrefix(String data) throws NoSuchCallbackDataException {
@@ -211,76 +188,94 @@ public class CheckPaymentsHandler implements CommandHandler {
         return data.split("/")[1];
     }
 
-    private List<BotApiMethod<?>> loadOlderPayment(Update update, String chatState) {
-        List<BotApiMethod<?>> apiMethods = new ArrayList<>();
-        Payment payment = null;
-        String[] stateParts = chatState.split("/");
-        if (stateParts.length < 2) {
-            try {
-                payment = paymentService.getFirstPaymentWithUserBefore(LocalDate.now(), getUsername(update));
-            } catch (PaymentNotFoundException e) {
-                e.printStackTrace();
-            }
-            if (payment == null) {
-                SendMessage message = new SendMessage(
-                        getChatId(update),
-                        MSG_NO_PAYMENTS_AT_ALL
-                );
-                apiMethods.add(message);
-                return apiMethods;
-            }
-        } else{
-            try {
-                payment = paymentService.getFirstPaymentWithUserBefore(Long.valueOf(stateParts[1]), getUsername(update));
-            } catch (PaymentNotFoundException e) {
-                e.printStackTrace();
-            }
-            if (payment == null) {
-                return goToPreviousPayment(update, LBL_NEWER_PAYMENT);
-            }
-        }
-        try {
-            personService.updatePersonBotChatState(
-                    getUsername(update),
-                    stateParts[0] + getPaymentIdPart(payment));
-        } catch (PersonNotFoundException e) {
-            e.printStackTrace();
-        }
-        return displayPaymentInfo(update, payment);
+    private List<BotApiMethod<?>> loadPreviousPayment() {
+        return updateKeyboard(currentPayment);
     }
 
-    private List<BotApiMethod<?>> displayPaymentInfo(Update update, Payment payment) {
+    private List<BotApiMethod<?>> toggleConfirmation() {
+        currentPayment.setIsConfirmed(!currentPayment.getIsConfirmed());
+        paymentService.updatePayment(currentPayment);
+        return updateKeyboard(currentPayment);
+    }
+
+    private List<BotApiMethod<?>> updateKeyboard(Payment nextPayment) {
         List<BotApiMethod<?>> apiMethods = new ArrayList<>();
-        String groupTitle = getGroupTitle(payment);
-        SendMessage message = new SendMessage(
-                getChatId(update),
-                GeneralPaymentInfoBuilder.of(payment, groupTitle)
-        );
-        message.setReplyMarkup(getNormalButtons(update, payment));
-        apiMethods.add(message);
+        EditMessageText editText = new EditMessageText();
+        editText
+                .setChatId(currentPerson.getChatId())
+                .setText(GeneralPaymentInfoBuilder.of(nextPayment, getGroupTitle(nextPayment)))
+                .setMessageId(currentUpdate.getCallbackQuery().getMessage().getMessageId());
+        EditMessageReplyMarkup editMarkup = new EditMessageReplyMarkup();
+        editMarkup
+                .setReplyMarkup(getKeyboardMarkup())
+                .setInlineMessageId(currentUpdate.getCallbackQuery().getInlineMessageId())
+                .setChatId(currentPerson.getChatId())
+                .setMessageId(currentUpdate.getCallbackQuery().getMessage().getMessageId());
+        apiMethods.add(editText);
+        apiMethods.add(editMarkup);
         return apiMethods;
     }
 
-    private InlineKeyboardMarkup getNormalButtons(Update update, Payment payment) {
-        InlineKeyboardMarkupBuilder builder = new InlineKeyboardMarkupBuilder();
-        if (payment.getRecipient().equals(getUsername(update))) {
-            String label = payment.getIsConfirmed() ? LBL_UNCONFIRM : LBL_CONFIRM;
-            builder.setButton(
-                    0, 0,
-                    label,
-                    CallbackData.TOGGLE_CONFIRMATION +
-                            getPaymentIdPart(payment));
+    private List<BotApiMethod<?>> loadOlderPayment() {
+        Payment nextPayment = null;
+        try {
+            nextPayment = paymentService
+                    .getFirstPaymentWithUserBefore(
+                            currentPayment.getPaymentId(),
+                            currentPerson.getUsername());
+        } catch (PaymentNotFoundException e) {
+            e.printStackTrace();
         }
-        builder
-                .setButton(1, 0,
-                        LBL_NEWER_PAYMENT,
-                        CallbackData.LOAD_NEWER_PAYMENT.getValue()
-                                + getPaymentIdPart(payment))
-                .setButton(1, 1,
-                        LBL_OLDER_PAYMENT,
-                        CallbackData.LOAD_OLDER_PAYMENT.getValue()
-                                + getPaymentIdPart(payment));
-        return builder.build();
+
+        if (nextPayment == null) {
+            return buildKeyboardWithOneButton(LBL_NEWER_PAYMENT);
+        }
+
+        updatePersonBotChatState(getPaymentIdPart(nextPayment));
+        return updateKeyboard(nextPayment);
+    }
+
+    private List<BotApiMethod<?>> loadNewerPayment() {
+        Payment nextPayment = null;
+        try {
+            nextPayment = paymentService.getFirstPaymentWithUserAfter(
+                    currentPayment.getPaymentId(),
+                    currentPerson.getUsername());
+        } catch (PaymentNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (nextPayment == null) {
+            return buildKeyboardWithOneButton(LBL_OLDER_PAYMENT);
+        }
+        updatePersonBotChatState(getPaymentIdPart(nextPayment));
+        return updateKeyboard(nextPayment);
+    }
+
+    private void updatePersonBotChatState(String state) {
+        try {
+            personService.updatePersonBotChatState(
+                    currentPerson.getUsername(),
+                    stateService.buildBotChatState(
+                            "checkpayments" + state));
+        } catch (PersonNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private List<BotApiMethod<?>> buildKeyboardWithOneButton(String buttonLabel) {
+        List<BotApiMethod<?>> apiMethods = new ArrayList<>();
+        SendMessage message = new SendMessage(
+                currentPerson.getChatId(),
+                MSG_NO_PAYMENTS
+        );
+        InlineKeyboardMarkupBuilder builder = new InlineKeyboardMarkupBuilder();
+        builder.setButton(0, 0,
+                buttonLabel,
+                CallbackData.LOAD_PREVIOUS_PAYMENT.getValue() +
+                        getPaymentIdPart(currentPayment));
+        message.setReplyMarkup(builder.build());
+        apiMethods.add(message);
+        return apiMethods;
     }
 
     private String getGroupTitle(Payment payment) {
@@ -296,43 +291,6 @@ public class CheckPaymentsHandler implements CommandHandler {
         return groupTitle;
     }
 
-    private List<BotApiMethod<?>> loadNewerPayment(Update update, String chatState) {
-        Payment payment = null;
-        String[] stateParts = chatState.split("/");
-        try {
-            payment = paymentService.getFirstPaymentWithUserAfter(Long.valueOf(stateParts[1]), getUsername(update));
-        } catch (PaymentNotFoundException e) {
-            e.printStackTrace();
-        }
-        if (payment == null) {
-            return goToPreviousPayment(update, LBL_OLDER_PAYMENT);
-        }
-        try {
-            personService.updatePersonBotChatState(
-                    getUsername(update),
-                    stateParts[0] + getPaymentIdPart(payment));
-        } catch (PersonNotFoundException e) {
-            e.printStackTrace();
-        }
-        return displayPaymentInfo(update, payment);
-    }
-
-    private List<BotApiMethod<?>> goToPreviousPayment(Update update, String label) {
-        List<BotApiMethod<?>> apiMethods = new ArrayList<>();
-        SendMessage message = new SendMessage(
-                getChatId(update),
-                MSG_NO_PAYMENTS
-        );
-        InlineKeyboardMarkupBuilder builder = new InlineKeyboardMarkupBuilder();
-        builder.setButton(0, 0,
-                label,
-                CallbackData.LOAD_PREVIOUS_PAYMENT.getValue() +
-                        getCallbackDataPostfix(update.getCallbackQuery().getData()));
-        message.setReplyMarkup(builder.build());
-        apiMethods.add(message);
-        return apiMethods;
-    }
-
     private String getPaymentIdPart(Payment payment) {
         return "/" + payment.getPaymentId();
     }
@@ -341,11 +299,6 @@ public class CheckPaymentsHandler implements CommandHandler {
         if (update.hasCallbackQuery())
             return update.getCallbackQuery().getFrom().getUserName();
         return update.getMessage().getFrom().getUserName();
-    }
-
-    private Long getChatId(Update update) {
-        return update.hasMessage() ?
-                update.getMessage().getChatId() : update.getCallbackQuery().getMessage().getChatId();
     }
 
     @Override
